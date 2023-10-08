2fedb70f5ecaba5ac03b4dccfbd64c0dff82fbfb
Jeyhun Abbasov <jeyhun.abbasov@evitec.com>
2023-10-08 22:30:00 +0300

    (TASK-41) Add Functionality to get jwt user

--- WebAPI/Controllers/LoginController.cs
+++ WebAPI/Controllers/LoginController.cs
@@ -1,9 +1,4 @@
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Text;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
 using WebAPI.Auth;
 
 namespace WebAPI.Controllers;
