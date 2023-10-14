d27f95c81a344ccf185700d7e2c4aae3e434de79
smrsgv <samir.isagov@hotmail.com>
2023-10-14 11:00:54 +0200

    (TASK-34) Refactor GetLibResponse

--- Application/Commons/Dtos/GetLibResponse.cs
+++ Application/Commons/Dtos/GetLibResponse.cs
@@ -1,6 +1,16 @@
 ﻿namespace Application.Commons.Dtos;
 
-public class GetLibResponse
+public record GetLibResponse
+{
+    public GetLibDto Data { get; set; } = new GetLibDto();
+}
+
+public record GetAllLibResponse
+{
+    public List<GetLibDto> Data { get; set; } = new List<GetLibDto>();
+}
+
+public record GetLibDto
 {
     public int Id { get; set; }
     public string Value { get; set; }
--- WebAPI/Controllers/CitizenshipsController.cs
+++ WebAPI/Controllers/CitizenshipsController.cs
@@ -5,29 +5,28 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace WebAPI.Controllers
-{
-    [Route("api/citizenships")]
-    [ApiController]
-    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-    public class CitizenshipsController : ControllerBase
-    {
-        private readonly IMapper _mapper;
-        private readonly IMediator _mediator;
+namespace WebAPI.Controllers;
 
-        public CitizenshipsController(IMapper mapper, IMediator mediator)
-        {
-            _mapper = mapper;
-            _mediator = mediator;
-        }
+[Route("api/citizenships")]
+[ApiController]
+[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+public class CitizenshipsController : ControllerBase
+{
+    private readonly IMapper _mapper;
+    private readonly IMediator _mediator;
+
+    public CitizenshipsController(IMapper mapper, IMediator mediator)
+    {
+        _mapper = mapper;
+        _mediator = mediator;
+    }
         
-        [HttpGet]
-        [Route("")]
-        public async Task<IActionResult> GetAllCountries(CancellationToken cancellationToken)
-        {
-            var request = new GetAllCitizenshipsRequest();
-            var response = await _mediator.Send(request, cancellationToken);
-            return Ok(response);
-        }
+    [HttpGet]
+    [Route("")]
+    public async Task<IActionResult> GetAllCountries(CancellationToken cancellationToken)
+    {
+        var request = new GetAllCitizenshipsRequest();
+        var response = await _mediator.Send(request, cancellationToken);
+        return Ok(response);
     }
 }
